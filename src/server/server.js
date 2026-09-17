9c83398be0d31d474460288563ccd22f5ad7304d
kamalesh <215050157+kamalesh-codes@users.noreply.github.com.>
2026-09-18 02:06:53 +0530

    Structure directory: move files to src/public and src/server

--- src/server/server.js
+++ src/server/server.js
@@ -5,17 +5,17 @@ const FileSync = require('lowdb/adapters/FileSync');
 const app = express();
 const PORT = 3000;
 
-const adapter = new FileSync('db.json');
+const adapter = new FileSync(path.join(__dirname, '../../db.json'));
 const db = low(adapter);
 
 // Set defaults
 db.defaults({ highScores: [] }).write();
 
 app.use(express.json());
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 app.get('/api/highscore', (req, res) => {
